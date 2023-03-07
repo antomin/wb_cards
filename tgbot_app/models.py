23f3c9164ed111785337ad068e0e6b864219be50
Anton Mindlin <anton@MacBook-Pro-Anton.local>
2023-03-07 12:57:57 +0200

    Models created.

--- tgbot_app/models.py
+++ tgbot_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class AppUser(models.Model):
+    id = models.PositiveIntegerField(verbose_name='телеграм id', primary_key=True, unique=True)
+    username = models.CharField(verbose_name='имя пользователя', max_length=32, unique=True)
+    name = models.CharField(verbose_name='имя', max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
+
+    def __str__(self):
+        return f'<User: {self.id}({self.username})>'
+
+
+class UserSession(models.Model):
+    user = models.ForeignKey(AppUser, verbose_name='пользователь', on_delete=models.PROTECT)
+    product_title = models.CharField(verbose_name='наименование товара', max_length=255)
+    product_description = models.TextField(verbose_name='описание товара')
+    product_characteristics = models.JSONField(verbose_name='характеристики товара')
+    is_active = models.BooleanField(verbose_name='открыта', default=True)
+    created_at = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
+
+    def __str__(self):
+        return f'<Session: {self.id}({self.product_title})>'
+
+
+class Message(models.Model):
+    user_session = models.ForeignKey(UserSession, verbose_name='сессия', on_delete=models.PROTECT)
+    is_answer = models.BooleanField(verbose_name='chatgpt', default=False)
+    text = models.TextField(verbose_name='сообщение')
+    created_at = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
+
+    def __str__(self):
+        return f'<Message: {self.id}({self.session_id})>'
+
+    class Meta:
+        ordering = ['created_at']
