56e26c730a6c0786fb1432448a72ba93a77c2885
Anton Mindlin <anton.mindlin@gmail.com>
2023-04-01 14:17:51 +0200

    Command dump_csv editet.

--- tgbot_app/management/commands/dump_csv.py
+++ tgbot_app/management/commands/dump_csv.py
@@ -8,10 +8,10 @@ from tgbot_app.models import SeoWB
 class Command(BaseCommand):
     def handle(self, *args, **options):
         queryset = SeoWB.objects.all()
-        with open('requests WB 01.02.2023.csv', 'w', encoding='utf-8') as file:
+        with open('dump.csv', 'w', encoding='utf-8') as file:
             writer = csv.writer(file, delimiter=',')
             cnt = 1
             for item in queryset:
-                writer.writerow([item.frase, item.frequency, item.lemmas])
+                writer.writerow([item.phrase, item.frequency, item.lemmas])
                 print(cnt)
                 cnt += 1
